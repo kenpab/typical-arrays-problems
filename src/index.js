fbdf212aecf6723debb5dd3f2f8046461239a6fd
kenpab <kenpazz@gmail.com>
2020-10-03 23:54:44 +0300

    feat: rewrite code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (!array || array.length === 0) {
+      return 0;
+    }
+    return Math.min.apply(null, array);
+  }
+  
+  exports.max = function max (array) {
+    if (!array || array.length === 0) {
+      return 0;
+    }
+    return Math.max.apply(null, array);
+  }
+  
+  exports.avg = function avg (array) {
+    if (!array || array.length === 0) {
+      return 0;
+    }
+    return array.reduce((x, y) => x + y, 0) / array.length;
+  } 
